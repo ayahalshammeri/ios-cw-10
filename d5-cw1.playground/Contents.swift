c030640036af61c963bfe5a2a6fc88febe34636b
AyahAlshammeri <66526830+jumana-alshameri@users.noreply.github.com>
2022-03-18 18:07:31 +0300

    done

--- d5-cw1.playground/Contents.swift
+++ d5-cw1.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+
+let family = ["juman","shehab","AYAH🌞","ali","muha","ibra"]
+print(family.count)
+
+var evenNumbers = [2,4,6,8,10,12]
+evenNumbers.remove(at: 2)
+evenNumbers += [16,18,20,22]
+
+var money = [1.23, 4.567, 8.90]
+money.randomElement()
+money.removeAll()
+
+var bbb = [1,2,3,4,5]
+for i in bbb {
+    print(i)
+}
+bbb.append += ("6","7","8","9","10")
+for y in bbb {
+    print(y)
+}
